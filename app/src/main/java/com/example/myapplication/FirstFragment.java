9458f8c5d7dfe877fd6bdf086ed6bbd490148342
Afaq Malik <mafaq6141@gmail.com>
2023-09-16 20:57:36 +0500

    wrong

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -22,7 +22,7 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot(;
+        return binding.getRoot();
 
     }
 
